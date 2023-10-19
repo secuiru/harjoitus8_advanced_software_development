9d4b96cbabd5646ea8dad6a3ebfb2c7ba8f001de
secuiru <vili.kauhanen@gmail.com>
2023-10-19 13:25:20 +0300

    fifth

--- harj8.py
+++ harj8.py
@@ -51,9 +51,23 @@ class Monkey_object:
   def __init__(self,monkey,island):
     self.monkey=monkey
     self.island = island
+    self.alive = 1
 
+global islandlist
+islandlist=[]
+class Island_object:
+  def __init__(self,x,y,occupant_amount,travelling,name):
+    self.x=x
+    self.y = y
+    self.name=name
+    self.occupant_amount = occupant_amount
+    self.travelling =travelling
+
+global island_number
+island_number=1
 def new_island():
     global monkeylist
+    global islandlist
     n=0
     count=0
     island_x=random.randint(0,519)
@@ -88,14 +102,22 @@ def new_island():
                 #print("arvotaan uusi sijainti")
 
         if area_empty == 1:
+            global island_number
+            travelling=0
+            if island_number==1:
+                travelling=1
+                
+            
             island_matrix[island_x,island_y]=1
+
+           
             island_x+=1
             count+=1
             if count >140: 
                 island_y+=1
                 count=0
                 island_x=ix
-                print("x= ",island_x,"y= ",island_y)
+                #print("x= ",island_x,"y= ",island_y)
             
             if island_y >= iy:
                 ax1m.set_data(island_matrix)
@@ -103,8 +125,14 @@ def new_island():
                 fig1.canvas.flush_events()
                 island_x=island_y-20
                 island_y=ix
+                print(island_number)
+                name="S%d"%(island_number)
+                island_number+=1
+                island =Island_object(x=island_x,y=island_y,name=tk.Label(ikkuna,text=name,background="yellow"),travelling=travelling,occupant_amount=10)
+                islandlist.append(island)
                 for i in range(10):
                     m = Monkey_object(monkey = tk.Label(ikkuna,image=monkey_img),island=1)
+
                     island_x-=20
                     count+=1
                     monkeylist.append(m)
@@ -113,8 +141,11 @@ def new_island():
                         island_y+=40
                         count=0
                     
-                    print("apina x= ",island_x,"y= ",island_y)
+                    #print("apina x= ",island_x,"y= ",island_y)
                     m.monkey.place(x=island_x,y=island_y)
+                island_y+=40
+                island_x+=70
+                island.name.place(x=island_x,y=island_y)
                 n=1
     
 
@@ -129,52 +160,71 @@ def monkey_checks():
     soundlist=[]
     while n==0:
         length = len(monkeylist)
-
+        length-=1
         for i in range(length):
             if soundlist.__len__() <length:
                     soundlist.append(random.randint(200,1000))
             winsound.Beep(soundlist[i],50)
 
         for i in range(length):
-            luckynumber=random.randint(0,10)
-            if luckynumber==1:
-                
-                if monkeylist[i].island==1:
-                    winsound.PlaySound("nauru.wav",winsound.SND_ASYNC)
-                if monkeylist[i].island==0:
-                    winsound.PlaySound("eat.wav",winsound.SND_ASYNC)
-                monkeylist[i].monkey.destroy()
-                monkeylist.pop(i)
-                soundlist.pop(i)
-        time.sleep(10)
+            if monkeylist[i].alive==1:
+                luckynumber=random.randint(0,1)
+                if luckynumber==1:
+                    monkeylist[i].alive=0
+                    if monkeylist[i].island==1:
+                        winsound.PlaySound("nauru.wav",winsound.SND_ASYNC)
+                    if monkeylist[i].island==0:
+                        winsound.PlaySound("eat.wav",winsound.SND_ASYNC)
+                    monkeylist[i].monkey.destroy()
+  
+        time.sleep(10)    
+
 
 def monkey_checks_thread():
     t=threading.Thread(target=monkey_checks)
     t.start()   
 
 def monkey_swim():
-    max=monkeylist.__len__()-1
-    i=random.randint(0,max)
-    monkeylist[i].island=0
-    x=monkeylist[i].monkey.place_info().get("x")
-    y=monkeylist[i].monkey.place_info().get("y")
-    count=0
-    x=int(x)
-    y=int(y) 
-    for j in range(18):
-        count+=1
-        if count < 10:
-            x+=10
-        if count >= 10:
-            x-=10
-        time.sleep(0.5)
+    alivelist=[]
+    max1=monkeylist.__len__()-1
+    for i in range(max1):
+        if monkeylist[i].alive==1:
+            alivelist.append(i)
+    max2=alivelist.__len__()-1
+    k=random.randint(0,max2)
+    print(k)
+    i=alivelist[k]
+    if monkeylist[i].island==0:
+        print("apina oli jo uimassa")
+    else:
         try:
-            monkeylist[i].monkey.place(x=x,y=y)
+            monkeylist[i].island=0
+            x=monkeylist[i].monkey.place_info().get("x")
+            y=monkeylist[i].monkey.place_info().get("y")
+            count=0
+            x=int(x)
+            orig_x=x
+            y=int(y) 
+            j=0
+            while count < 19:
+                if count < 10:
+                    x+=10
+                if count >= 10:
+                    x-=10
+                monkeylist[i].monkey.place(x=x,y=y)
+                time.sleep(0.5)
+                count+=1
+                
+            #x=orig_x   
+            monkeylist[i].monkey.place(x=orig_x,y=y) 
+            monkeylist[i].island=1
         except:
             print("apina menehtyi merellä")
-            break
-            
-    monkeylist[i].island=1
+           
+    
+        
+    
+    
 def monkey_swim_thread():
     t=threading.Thread(target=monkey_swim)
     t.start()       
@@ -183,6 +233,9 @@ def delete_island():
     global monkeylist
     global island_matrix
     global soundlist
+    global islandlist
+    global island_number
+    island_number=1
     for j in range(700):
      for i in range(700):
          island_matrix[j,i]=-10
@@ -194,6 +247,14 @@ def delete_island():
         
     monkeylist.clear()    
     soundlist.clear()
+
+    length = len(islandlist)
+  
+    for i in range(length):
+        islandlist[i].name.destroy()
+        
+    islandlist.clear()    
+    
 
     ax1m.set_data(island_matrix)
     fig1.canvas.draw()
@@ -208,14 +269,13 @@ add_island.place(x=50,y=50)
 delete_island_button = tk.Button(ikkuna, text ="DELETE ISLAND", command = delete_island_thread)
 delete_island_button.place(x=50,y=80)
 
-delete_monkey_button = tk.Button(ikkuna, text ="DELETE MONKEY", command = monkey_checks_thread)
-delete_monkey_button.place(x=120,y=80)
+#delete_monkey_button = tk.Button(ikkuna, text ="DELETE MONKEY", command = monkey_checks_thread)
+#delete_monkey_button.place(x=120,y=80)
 
 delete_monkey_button = tk.Button(ikkuna, text ="SWIM", command = monkey_swim_thread)
 delete_monkey_button.place(x=120,y=50)
 
-#monkey = tk.Label(ikkuna,image=monkey_img)
-#monkey.place(x=200,y=200)
+
 monkey_checks_thread()
 
 
